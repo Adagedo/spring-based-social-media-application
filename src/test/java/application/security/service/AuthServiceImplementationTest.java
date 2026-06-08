b8ad973fadfb6823a02265116e581d1c883aac27
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-06-08 13:24:42 +0100

    authentication tests

--- src/test/java/application/security/service/AuthServiceImplementationTest.java
+++ src/test/java/application/security/service/AuthServiceImplementationTest.java
@@ -1,5 +1,8 @@
 package application.security.service;
 
+import application.dto.requestDto.SignInRequest;
+import application.dto.requestDto.SignUpRequest;
+import application.dto.responseDto.SignInResponse;
 import application.entity.user.UserEntity;
 import application.transactions.auth.CreateUserTransaction;
 import application.transactions.auth.VerifyCodeTransaction;
@@ -8,14 +11,19 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class AuthServiceImplementationTest {
 
@@ -49,7 +57,14 @@ class AuthServiceImplementationTest {
     }
 
     @Test
-    void signUp() {
+    void ShouldSignUpNewUser() {
+
+        int verification_code = (int) ((Math.random() * 900000) + 100000);
+        SignUpRequest request = new SignUpRequest(
+                "Donald Trump",
+                "traump@gmail.com",
+                "Donald@34413!##121"
+        );
 
         UserEntity user = UserEntity.builder()
                 .username("Trump")
@@ -57,38 +72,171 @@ class AuthServiceImplementationTest {
                 .password("Trump1234!@#$")
                 .build();
 
+        UserEntity user_ = null;
         when(createUserTransaction.findUserByEmail(user.getEmail())).thenReturn(Optional.of(user));
-        assertAll(
-                "users",
-                () -> {
-                    assertNotNull(user);
-                    String username = user.getUsername();
-                    assertNotNull(username);
-                    assertAll(
-                            "username",
-                            () -> assertEquals(username, user.getUsername())
-                    );
+        Optional<UserEntity> optionalUser = this.createUserTransaction.findUserByEmail(request.email());
+        if (optionalUser.isPresent()){
+            user_ = optionalUser.get();
+        }
+        assertNull(user_);
+        ResponseEntity<?> response = authServiceImplementation.signUp(request);
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(
+                "User registered successfully. Please verify your email.",
+                response.getBody()
+        );
+        verify(createUserTransaction, times(1)).saveUserAfterSingUp(request);
 
-                },
-                () -> {
-                    String email = user.getEmail();
-                    assertAll(
-                            "email",
-                            () -> assertNotNull(email),
-                            () -> assertEquals(email, user.getEmail())
-                    );
-                }
+    }
+
+    @Test
+    void ShouldSendVerificationCodeToExistingUsers() {
+        int verification_code = (int) ((Math.random() * 900000) + 100000);
+        SignUpRequest request = new SignUpRequest(
+                "Donald Trump",
+                "traump@gmail.com",
+                "Donald@34413!##121"
+        );
+
+        UserEntity user = UserEntity.builder()
+                .username("Donald Trump")
+                .email("traump@gmail.com")
+                .password("Donald@34413!##121")
+                .build();
+
+        UserEntity user_ = null;
+        when(createUserTransaction.findUserByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        Optional<UserEntity> optionalUser = this.createUserTransaction.findUserByEmail(request.email());
+        if (optionalUser.isPresent()){
+            user_ = optionalUser.get();
+        }
+        assertNotNull(user_);
+        assertFalse(user_.is_verified());
+        ResponseEntity<?> response = authServiceImplementation.signUp(request);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(
+                "User exists but is unverified. A new verification email has been sent.",
+                response.getBody()
+        );
+
+
+    }
+
+    @Test
+    void ShouldNotSignUpAlreadyRegisteredAndVerifiedUsers() {
+        SignUpRequest request = new SignUpRequest(
+                "Donald Trump",
+                "traump@gmail.com",
+                "Donald@34413!##121"
+        );
+
+        UserEntity user = UserEntity.builder()
+                .username("Donald Trump")
+                .email("traump@gmail.com")
+                .password("Donald@34413!##121")
+                .is_verified(true)
+                .build();
+
+        UserEntity user_ = null;
+        when(createUserTransaction.findUserByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        Optional<UserEntity> optionalUser = this.createUserTransaction.findUserByEmail(request.email());
+        if (optionalUser.isPresent()){
+            user_ = optionalUser.get();
+        }
+        assertNotNull(user_);
+        assertTrue(user_.is_verified());
+        ResponseEntity<?> response = authServiceImplementation.signUp(request);
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+        assertEquals(
+                "Error: This email is already registered and verified.",
+                response.getBody()
         );
     }
 
     @Test
-    void signIn() {
+    void shouldLoginSuccessfully() {
+
+        SignInRequest request =
+                new SignInRequest("Joe@gmail.com", "password");
+
+        UserEntity user = UserEntity.builder()
+                .username("Joe")
+                .email("Joe@gmail.com")
+                .password(passwordEncoder.encode(request.password()))
+                .status("active")
+                .is_verified(true)
+                .build();
+
+        Authentication authentication =
+                mock(Authentication.class);
+
+        when(createUserTransaction.findUserByEmail("Joe@gmail.com"))
+                .thenReturn(Optional.of(user));
+
+        when(passwordEncoder.matches(
+                "password",
+                user.getPassword()
+        )).thenReturn(true);
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+
+        when(authentication.isAuthenticated()).thenReturn(true);
+
+        when(jwtService.generateToken(user.getUsername(), String.valueOf(user.getId()))).thenReturn(any(String.class));
+
+        ResponseEntity<?> response = authServiceImplementation.signIn(request);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        assertEquals(new SignInResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getStatus()
+        ), response.getBody());
+        var body =  response.getBody();
+
+        assertNotNull(body);
+//        assertEquals(user.getUsername(), body();
+
+        verify(jwtService, times(1)).generateToken(user.getUsername(), String.valueOf(user.getId()));
+
+        verify(createUserTransaction).UpdateUserStatus(user);
+    }
+    @Test
+    void shouldReturnUnauthorizedWhenPasswordIsInvalid() {
+
+        SignInRequest request =
+                new SignInRequest("Joe@gmail.com", "wrong-password");
+
+        UserEntity user = UserEntity.builder()
+                .email("Joe@gmail.com")
+                .password("encoded-password")
+                .build();
+
+        when(createUserTransaction.findUserByEmail("Joe@gmail.com"))
+                .thenReturn(Optional.of(user));
+
+        when(passwordEncoder.matches(
+                "wrong-password",
+                "encoded-password"
+        )).thenReturn(false);
+
+        ResponseEntity<?> response = authServiceImplementation.signIn(request);
+
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+        assertEquals("invalid credentials", response.getBody());
+
+        verify(authenticationManager, never())
+                .authenticate(any());
     }
 
     @Test
     void verifyCode() {
+
     }
 
+    
     @Test
     void logOut() {
     }
