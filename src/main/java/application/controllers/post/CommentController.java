cfcc02164f8040a7d7837ff9d3d4d9591964cde7
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-06-21 09:51:27 +0100

    updated api convension

--- src/main/java/application/controllers/post/CommentController.java
+++ src/main/java/application/controllers/post/CommentController.java
@@ -1,14 +1,22 @@
 package application.controllers.post;
 
-import application.dto.requestDto.CommentRequestDto;
-import application.service.comment.CommentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import application.dto.requestDto.CommentRequestDto;
+import application.service.comment.CommentService;
 
 @RestController
-@RequestMapping("/api/v1/post/comment")
+@RequestMapping("/api/v1/users/posts/")
 public class CommentController {
 
     private final CommentService commentService;
@@ -22,33 +30,33 @@ public class CommentController {
         return this.commentService.createComment(post_id, requestDto, userDetails);
     }
 
-    @PostMapping("/update/{comment_id}")
+    @PostMapping("/{post_id}/{comment_id}")
     public ResponseEntity<?> updateComment(@PathVariable String comment_id, @ModelAttribute CommentRequestDto requestDto, @AuthenticationPrincipal UserDetails userDetails){
         return this.commentService.editComment(comment_id, requestDto, userDetails);
     }
 
-    @GetMapping("/{post_id}")
+    @GetMapping("/{post_id}/comments")
     public ResponseEntity<?> getPostComment(@PathVariable String post_id, @AuthenticationPrincipal UserDetails userDetails){
         return this.commentService.getAllCommentsAssociatedToAPost(post_id, userDetails);
     }
 
-    @DeleteMapping("/delete/{comment_id}")
+    @DeleteMapping("/{post_id}/{comment_id}")
     public ResponseEntity<?> deleteComment(@PathVariable String comment_id, @AuthenticationPrincipal UserDetails userDetails){
         return this.commentService.deleteComment(comment_id, userDetails);
     }
 
-    @PostMapping("/reply/{parent_comment_id}")
+    @PostMapping("/{post_id}/reply/{parent_comment_id}")
     public ResponseEntity<?> replyToPost(@PathVariable String parent_comment_id, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetails userDetails){
         return this.commentService.createReply(parent_comment_id, requestDto, userDetails);
     }
 
-    @PostMapping("/reply/update/{parent_comment_id}")
+    @PostMapping("/{post_id}/reply/{parent_comment_id}")
     public ResponseEntity<?> updateReply(@PathVariable String parent_comment_id, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetails userDetails){
         return this.commentService.editReply(parent_comment_id, requestDto, userDetails);
     }
 
-    @DeleteMapping("/reply/{parent_comment_id}")
-    public ResponseEntity<?> deleReply(@PathVariable String parent_comment_id, @AuthenticationPrincipal UserDetails userDetails){
+    @DeleteMapping("/{post_id}/comments/replies/{parent_comment_id}")
+    public ResponseEntity<?> deleteReply(@PathVariable String parent_comment_id, @AuthenticationPrincipal UserDetails userDetails){
         return this.commentService.deleteReply(parent_comment_id, userDetails);
     }
 }
